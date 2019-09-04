98015003f6149ea12a5cc72978a0f0c8f911790d
vagrant <Kennethzhang374@gmail.com>
2019-09-04 23:04:19 +0000

    skip username part since i did that before and confusing move to setup user database

--- express_server.js
+++ express_server.js
@@ -12,6 +12,18 @@ const randomstring = require("randomstring");
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 /////////////////////////////////////
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+} 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
