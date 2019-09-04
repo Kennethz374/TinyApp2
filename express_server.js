e6dbae61015cb9015704375ec50f67f3c40294e6
vagrant <Kennethzhang374@gmail.com>
2019-09-04 20:48:55 +0000

    added a second route that display single url and its shortened form

--- express_server.js
+++ express_server.js
@@ -8,10 +8,15 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//main page that shows all the short urls and long urls (like the urlDB)
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase }; //pass down the DB to ejs 
   res.render("urls_index", templateVars);
+});
+//2nd Route
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars); //pass down the info short and original urls to ejs
 });
 
 app.listen(PORT, () => {
